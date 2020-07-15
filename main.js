8e6d35ef4644e586746919b293eeb19325bcf72f
Akash <pathakvikash9211@gmail.com>
2020-07-15 15:09:38 +0530

    Added blog validation function and set at localstorage

--- main.js
+++ main.js
@@ -42,5 +42,43 @@ registerUser("pathakvikash@9211", "1234");
 loginUser("pathakvikash@9211", "12345");
 
 function addBlog(blog) {
-  blogs.push(blog);
+  if (validateBlog(blog)) {
+    let blogPosts = JSON.parse(localStorage.getItem("blogs")) || [];
+    blogPosts.push(blog);
+    blogs.push(blog);
+    localStorage.setItem("blogs", JSON.stringify(blogPosts));
+  } else {
+    return;
+  }
 }
+
+function validateBlog(blog) {
+  let ok = true;
+  if (blog.title == "") {
+    console.log("Need a title");
+    ok = false;
+  }
+  if (blog.blog == "") {
+    ok = false;
+    console.log("Need your thoughts");
+  }
+  if (blog.photo == "") {
+    ok = false;
+    console.log("Need photo");
+  }
+  return ok;
+}
+
+function getLoginUser() {
+  return JSON.parse(localStorage.getItem("loginUser"))[0];
+}
+
+let blog = new Blog({
+  id: blogs.length + 1,
+  title: "This is title",
+  blog: "This is description",
+  photo: "This is photo",
+  user: getLoginUser(),
+});
+
+addBlog(blog);
