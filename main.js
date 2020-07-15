4c1ebad667b9416a584b9a45d4358404125e3d5e
Akash <pathakvikash9211@gmail.com>
2020-07-16 04:26:34 +0530

    Fixes redirection bug

--- main.js
+++ main.js
@@ -1,50 +1,21 @@
-const blogs = [];
-
-function addBlog(blog) {
-  if (validateBlog(blog)) {
-    let blogPosts = JSON.parse(localStorage.getItem("blogs")) || [];
-    blogPosts.push(blog);
-    blogs.push(blog);
-    localStorage.setItem("blogs", JSON.stringify(blogPosts));
-  } else {
-    return;
-  }
+function getlcoalUser() {
+  return JSON.parse(localStorage.getItem("lcoalUser")) || false;
 }
-
-function validateBlog(blog) {
-  let ok = true;
-  if (blog.title == "") {
-    console.log("Need a title");
-    ok = false;
-  }
-  if (blog.blog == "") {
-    ok = false;
-    console.log("Need your thoughts");
-  }
-  if (blog.photo == "") {
-    ok = false;
-    console.log("Need photo");
-  }
-  return ok;
-}
-
-function getLoginUser() {
-  return JSON.parse(localStorage.getItem("loginUser"))[0];
-}
-
-// let blog = new Blog({
-//   id: blogs.length + 1,
-//   title: "This is title",
-//   blog: "This is description",
-//   photo: "This is photo",
-//   user: getLoginUser(),
-// });
-
-// addBlog(blog);
-
-window.onload = () => {
-  let loginUser = localStorage.getItem("loginUser");
-  if (!loginUser) {
+let lcoalUser = getlcoalUser();
+if (!lcoalUser) {
+  if (window.location.href.indexOf("login.html") == -1) {
     window.location.href = "login.html";
   }
-};
+}
+function generateError(message) {
+  let errorMsg = document.getElementById("errorMsg");
+  errorMsg.innerHTML = `<h5>${message}</h5>`;
+  errorMsg.setAttribute("class", "p-5 fixed-top badge badge-danger d-block");
+  setTimeout(() => {
+    window.location.reload();
+  }, 3000);
+}
+
+// window.addEventListener("load", () => {
+
+// });
